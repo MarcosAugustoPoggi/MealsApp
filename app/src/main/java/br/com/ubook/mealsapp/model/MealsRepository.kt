84993d2356b6dedb4dfbba447e096a3412dc712f
marcosp-26784 <marcos.poggi@ubook.com>
2021-10-20 17:04:49 -0300

    Mvvm Get from API with retrofit

--- app/src/main/java/br/com/ubook/mealsapp/model/MealsRepository.kt
+++ app/src/main/java/br/com/ubook/mealsapp/model/MealsRepository.kt
@@ -1,4 +1,28 @@
 package br.com.ubook.mealsapp.model
 
-class MealsRepository {
+import br.com.ubook.mealsapp.model.api.MealsWeService
+import br.com.ubook.mealsapp.model.response.MealsCategoryResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MealsRepository(private val webService: MealsWeService = MealsWeService()) {
+    fun getMeals(successCallback: (response: MealsCategoryResponse?) -> Unit) {
+
+        return webService.getMeals().enqueue(object : Callback<MealsCategoryResponse> {
+            override fun onResponse(
+                call: Call<MealsCategoryResponse>,
+                response: Response<MealsCategoryResponse>
+            ) {
+                if (response.isSuccessful) {
+                    successCallback(response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<MealsCategoryResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 }
--- app/src/main/java/br/com/ubook/mealsapp/ui/meals/MainActivity.kt
+++ app/src/main/java/br/com/ubook/mealsapp/ui/meals/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import br.com.ubook.mealsapp.model.response.MealResponse
 import br.com.ubook.mealsapp.ui.theme.MealsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +35,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    Text(text = "Hello Compose!")
+    val remenberedMeals : MutableState<List<MealResponse>> = remember { mutableStateOf(emptyList<MealResponse>()) }
+    viewModel.getMeals { response ->
+        val mealsFromApi = response?.categories
+        remenberedMeals.value = mealsFromApi.orEmpty()
+    }
+    LazyColumn {
+        items(remenberedMeals.value) { meal ->
+            Text(text = meal.name)
+        }
+    }
 }
 
 @Preview(showBackground = true)
